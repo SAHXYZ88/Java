9973e27220b81b4248a4a2e4c13e4b0925d89f83
SAH <98654558+SAHXYZ88@users.noreply.github.com>
2026-06-06 16:24:06 +0530

    Create SearchById.java

---
+++ Collections/ArrayList/SearchById.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Employee {
+    private String name;
+    private int id;
+
+    Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    void display() {
+        System.out.println("Name : " + name);
+        System.out.println("Id : " + id);
+    }
+}
+
+class Developer extends Employee {
+    Developer(String name, int id) {
+        super(name, id);
+    }
+
+    @Override
+    void display() {
+        System.out.println("-: Developer :-");
+        super.display();
+    }
+}
+
+class Manager extends Employee {
+    Manager(String name, int id) {
+        super(name, id);
+    }
+    
+    @Override
+    void display() {
+        System.out.println("-: Manager :-");
+        super.display();
+    }
+}
+public class SearchByName {
+    public static void main(String[] args) {
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        employees.add(new Developer("SAH", 213));
+        employees.add(new Manager("Shabnur", 01));
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Id You Want To Search : ");
+        int searchId = sc.nextInt();
+
+        for (Employee employee : employees) {
+            if (employee.getId() == searchId) {
+                System.out.println("Employee Found");
+                employee.display();
+            }
+        }  
+    }
+}
