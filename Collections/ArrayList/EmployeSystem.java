3af17951c8048560140eff1e3ac4ae342709ff74
SAH <98654558+SAHXYZ88@users.noreply.github.com>
2026-06-06 07:23:00 +0530

    Create EmployeSystem.java

---
+++ Collections/ArrayList/EmployeSystem.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Employee {
+    String name;
+    int id;
+
+    Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+    void display() {
+        System.out.println("Name : " + name);
+        System.out.println("Id : " + id);
+    }
+}
+
+class Developer extends Employee {
+    Developer(String name, int id) {
+        super(name, id);
+    }
+
+    @Override
+    void display() {
+        System.out.println("-: Developer :-");
+        super.display();
+    }
+}
+
+class Manager extends Employee {
+    Manager(String name, int id) {
+        super(name, id);
+    }
+    
+    @Override
+    void display() {
+        System.out.println("-: Manager :-");
+        super.display();
+    }
+}
+
+public class EmployeSystem{
+    public static void main(String[] args) {
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        employees.add(new Developer("SAH", 213));
+        employees.add(new Manager("Shabnur", 01));
+
+        for (Employee employee : employees) {
+            employee.display();
+        }
+    }
+}
