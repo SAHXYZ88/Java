3c35a41695c79aba0da16f268c61131bbb4ddbf8
SAH <98654558+SAHXYZ88@users.noreply.github.com>
2026-06-07 06:32:47 +0530

    Create HashMapOfObjects.java

---
+++ Collections/HashMap/HashMapOfObjects.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+
+class Employee {
+    private String name;
+    private int id;
+
+    Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+    void display() {
+        System.out.println("Name : " + name);
+        System.out.println("Id : " + id);
+    }
+}
+
+class Developer extends Employee {
+    Developer(String name, int id) {
+        super(name, id);
+    }
+
+    @Override
+    void display() {
+        System.out.println("-: Developer :-");
+        super.display();
+    }
+}
+
+class Manager extends Employee {
+    Manager(String name, int id) {
+        super(name, id);
+    }
+    
+    @Override
+    void display() {
+        System.out.println("-: Manager :-");
+        super.display();
+    }
+}
+public class HashMapOfObjects {
+    public static void main(String[] args) {
+        HashMap<Integer, Employee> employees = new HashMap<>();
+
+        employees.put(1,new Developer("SAH", 1));
+        employees.put(2, new Manager("Shabnur", 2));
+        
+        for (int emp : employees.keySet()) {
+            employees.get(emp).display();
+        }
+    }
+}
