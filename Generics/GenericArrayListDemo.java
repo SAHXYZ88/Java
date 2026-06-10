96557bda84bf69191973213f9ab349dcca485fc6
SAH <98654558+SAHXYZ88@users.noreply.github.com>
2026-06-10 06:44:10 +0530

    Create GenericArrayListDemo.java

---
+++ Generics/GenericArrayListDemo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class GenericArrayListDemo{
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("SAH");
+        names.add("Zaid");
+
+        System.out.println(names.get(0));
+    }
+}
